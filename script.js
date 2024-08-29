926060f7052d9ba9672b4248af0cf5c9ced32e0b
Matt Nguyen <mattn9091@gmail.com>
2024-08-29 17:04:41 -0400

    Add function to add book cards

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const dialog = document.querySelector("dialog");
+const dialog = document.getElementById("myDialog");
 const showButton = document.querySelector('.add');
+const submitButton = document.getElementById('submit');
 const closeButton = document.querySelector('.closeDialog');
 const bookInput = document.getElementById('bookName');
+const authorInput = document.getElementById('author');
+const pagesInput = document.getElementById('pages');
 
 showButton.addEventListener("click", () => {
     dialog.showModal();
@@ -10,6 +13,38 @@ showButton.addEventListener("click", () => {
 closeButton.addEventListener('click', ()=>{
     dialog.close();
     bookInput.value = null;
-
+    authorInput.value = null;
+    pagesInput.value = null;
 });
 
+submitButton.addEventListener('click', ()=>{
+    let myBook = new Book(bookInput.value,authorInput.value,pagesInput.value, false);
+    createCard(myBook);
+    dialog.close();
+    bookInput.value = null;
+    authorInput.value = null;
+    pagesInput.value = null;
+});
+
+function Book(bookTitle, author, pages, read){
+    this.bookTitle = bookTitle;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function createCard(book){
+    const cardContainer = document.querySelector('.bookCardContainer');
+    const newCard = document.createElement('div');
+    newCard.classList.add('bookCard');
+    const title = document.createElement('p');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    title.textContent = book.bookTitle;
+    author.textContent = book.author;
+    pages.textContent = book.pages;
+    newCard.appendChild(title);
+    newCard.appendChild(author);
+    newCard.appendChild(pages);
+    cardContainer.appendChild(newCard);
+}
