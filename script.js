8fb4f53cba1468da00ea749fbc6c38b3af70358b
Matt Nguyen <mattn9091@gmail.com>
2024-08-29 19:01:24 -0400

    Add button to remove book card

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const closeButton = document.querySelector('.closeDialog');
 const bookInput = document.getElementById('bookName');
 const authorInput = document.getElementById('author');
 const pagesInput = document.getElementById('pages');
+const boolRead = document.getElementById('boolRead');
 
 showButton.addEventListener("click", () => {
     dialog.showModal();
@@ -18,8 +19,8 @@ closeButton.addEventListener('click', ()=>{
 });
 
 submitButton.addEventListener('click', ()=>{
-    let myBook = new Book(bookInput.value,authorInput.value,pagesInput.value, false);
-    createCard(myBook);
+    let myBook = new Book(bookInput.value,authorInput.value,pagesInput.value, boolRead.value);
+    myBook.createCard();
     dialog.close();
     bookInput.value = null;
     authorInput.value = null;
@@ -33,18 +34,38 @@ function Book(bookTitle, author, pages, read){
     this.read = read;
 }
 
-function createCard(book){
+Book.prototype.createCard = function() {
     const cardContainer = document.querySelector('.bookCardContainer');
     const newCard = document.createElement('div');
     newCard.classList.add('bookCard');
     const title = document.createElement('p');
     const author = document.createElement('p');
     const pages = document.createElement('p');
-    title.textContent = book.bookTitle;
-    author.textContent = book.author;
-    pages.textContent = book.pages;
+    const deleteBtn = document.createElement('button');
+    const readBtn = document.createElement('button');
+    title.textContent = this.bookTitle;
+    title.classList.add('cardTitle')
+    author.textContent ='by ' + this.author;
+    pages.textContent = 'Number of pages: ' + this.pages;
+    deleteBtn.textContent = 'Remove Book';
+    deleteBtn.classList.add('deleteButton');
+    readBtn.classList.add('readButton');
+    if(this.read = "true"){
+        readBtn.textContent = "Mark as not read";
+        newCard.style.backgroundColor = 'rgb(136,229,153)';
+        deleteBtn.style.backgroundColor = 'rgb(136,229,153)';
+    }
+    else{
+        readBtn.textContent = "Mark as read"
+    }
     newCard.appendChild(title);
     newCard.appendChild(author);
     newCard.appendChild(pages);
+    newCard.appendChild(readBtn);
+    newCard.appendChild(deleteBtn);
     cardContainer.appendChild(newCard);
+    deleteBtn.addEventListener('click', ()=>{
+        cardContainer.removeChild(newCard);
+    });
+
 }
